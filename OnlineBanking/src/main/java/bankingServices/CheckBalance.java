ece05aad6aeb910ff189c82ab211a2cd0b2d3517
Pavankalyansemiperfomance <117610062+Pavankalyansemiperfomance@users.noreply.github.com>
2024-04-29 07:42:22 +0530

    Add files via upload

---
+++ OnlineBanking/src/main/java/bankingServices/CheckBalance.java
@@ -0,0 +1,63 @@
+package bankingServices;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/CheckBalance")
+public class CheckBalance extends HttpServlet
+{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		String url="jdbc:mysql://localhost:3306?user=root&password=12345";
+		String query="select * from pavan1999.banking where accno=?";
+		String acn=req.getParameter("ac");
+		PrintWriter writer=resp.getWriter();
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection=DriverManager.getConnection(url);
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setString(1, acn);
+			ResultSet rs=ps.executeQuery();
+			if(rs.next())
+			{
+				String bal=rs.getString("accbal");
+				writer.println("<h1>Available Balance :</h1>"+bal);
+				writer.println("<br></br><input type='button' value='Go To Services' onclick='myFunction()'></input> <script >\r\n"
+						+ "		function myFunction()\r\n"
+						+ "		{\r\n"
+						+ "			window.location.href=\"Services.html\";\r\n"
+						+ "		}\r\n"
+						+ "	</script>");
+				writer.println("<br></br><input type='button' value='LogOut' onclick='myFunction()'></input> <script >\r\n"
+						+ "		function myFunction()\r\n"
+						+ "		{\r\n"
+						+ "			window.location.href=\"Homepage.html\";\r\n"
+						+ "		}\r\n"
+						+ "	</script>");
+			}
+			else
+			{
+				RequestDispatcher rd=req.getRequestDispatcher("ViewBalance.html");
+				rd.include(req, resp);
+				writer.println("Please Enter Correct Account Number..");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
